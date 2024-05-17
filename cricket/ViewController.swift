4bc88d27cafee615c5968249d29cb6e210ce9b2f
mslfh <1937154905@qq.com>
2024-05-17 12:59:31 +1000

    Initial Commit

---
+++ cricket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cricket
+//
+//  Created by mobiledev on 17/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
