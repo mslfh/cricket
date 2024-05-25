2c1e1368ab5e44494ef3e74659da58d0c8d7ed80
mslfh <1937154905@qq.com>
2024-05-25 22:00:19 +1000

    done

---
+++ cricket/SelectPlayerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SelectPlayerTableViewCell.swift
+//  cricket
+//
+//  Created by mobiledev on 25/5/2024.
+//
+
+import UIKit
+
+class SelectPlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ cricket/SelectTeamTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SelectTeamTableViewCell.swift
+//  cricket
+//
+//  Created by mobiledev on 25/5/2024.
+//
+
+import UIKit
+
+class SelectTeamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
